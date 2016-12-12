74607b0d90bd7ffa8d808a5de4bfbe052bdfa682
Abhishek Vasu <abhishekvasu@Abhisheks-MacBook-Pro.local>
2016-12-12 15:02:43 -0500

    Test code highlighting the problem

--- Wav Files- Bread/test.py
+++ Wav Files- Bread/test.py
@@ -7,12 +7,13 @@ import abcd
 
 # use a loop:
 
-wav_list = ['pronunciation _en_bread(1).wav', 'pronunciation _en_bread(2).wav']
+wav_list = ['pronunciation _en_bread(1).wav', 'pronunciation _en_bread(2).wav', 'pronunciation _en_bread(3).wav', 'pronunciation _en_bread(4).wav', 'pronunciation _en_bread(5).wav', 'pronunciation _en_bread(6).wav', 'pronunciation _en_bread(7).wav', 'pronunciation _en_bread(8).wav', 'pronunciation _en_bread(9).wav', 'pronunciation _en_bread(10).wav', 'pronunciation _en_bread(11).wav', 'pronunciation _en_bread(12).wav']
 for wav_file in wav_list:
 	wf = wave.open(wav_file)
 	fs, data = wavfile.read(wav_file) # load the data
-	X = np.fft.fft(data,16384)
-	print(X)
+	print(wf.getsampwidth())
+	# X = np.fft.fft(data,16384)
+	# print()
 	# abcd.plotstft(wav_file)
 	# plt.plot(abs(X)/max(abs(X)))
 	# plt.show()
