3b0794ebf27ba5b3280b8cdfb22815571c40c046
Abhishek Vasu <abhishekvasu@Abhisheks-MacBook-Pro.local>
2016-12-13 00:55:41 -0500

    Removed metadata from Bread wav files

--- Wav Files- Bread/test.py
+++ Wav Files- Bread/test.py
@@ -3,16 +3,17 @@ import wave
 import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt 
-import abcd
+# import abcd
 
 # use a loop:
 
-wav_list = ['pronunciation _en_bread(1).wav']
+wav_list = ['pronunciation_en_bread (1).wav', 'pronunciation_en_bread (2).wav', 'pronunciation_en_bread (3).wav', 'pronunciation_en_bread (4).wav', 'pronunciation_en_bread (5).wav', 'pronunciation_en_bread (6).wav', 'pronunciation_en_bread (7).wav', 'pronunciation_en_bread (8).wav', 'pronunciation_en_bread (9).wav', 'pronunciation_en_bread (10).wav', 'pronunciation_en_bread (11).wav', 'pronunciation_en_bread (12).wav']
 for wav_file in wav_list:
 	wf = wave.open(wav_file)
 	fs, data = wavfile.read(wav_file) # load the data
 	# print(wf.getsampwidth())
 	X = np.fft.fft(data,16384)
+	print(len(data))
 	# print()
 	# abcd.plotstft(wav_file)
 	plt.plot(abs(X)/max(abs(X)))
